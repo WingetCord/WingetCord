dbac02ec136a180e94ad13fe20b233799e0d11bd
AkramStation <zinoutv951@gmail.com>
2026-03-10 19:56:03 +0100

    added docs

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ export * from './core/CacheManager.js';
 export * from './core/EventManager.js';
 export * from './core/PluginManager.js';
 export * from './core/InteractionManager.js';
+export * from './core/HandlerManager.js';
 export * from './voice/VoiceManager.js';
 export * from './voice/AudioPlayer.js';
 export { InteractionHandler } from './core/HandlerManager.js';
@@ -19,6 +20,17 @@ export * from './structures/Role.js';
 export * from './structures/Member.js';
 export * from './structures/Guild.js';
 
+export * from './rest/GuildsHandler.js';
+export * from './rest/ChannelsHandler.js';
+export * from './rest/UsersHandler.js';
+export * from './rest/EmojiHandler.js';
+export * from './rest/StickerHandler.js';
+export * from './rest/CommandsHandler.js';
+export * from './rest/WebhooksHandler.js';
+export * from './rest/AuditLogsHandler.js';
+export * from './rest/AutoModHandler.js';
+export * from './rest/ScheduledEventsHandler.js';
+
 export * from './utils/EmbedBuilder.js';
 export * from './utils/ComponentBuilders.js';
 export * from './utils/Validator.js';
@@ -30,5 +42,8 @@ export * from './utils/BitField.js';
 export * from './utils/ModalBuilder.js';
 export * from './utils/Collection.js';
 export * from './utils/Color.js';
+export * from './utils/ContextMenuBuilders.js';
+export * from './utils/Permissions.js';
+export * from './utils/DiscordHelpers.js';
 
 export * from './types/index.js';
